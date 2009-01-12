8992f3c3d50efb528de8781a0e188ef2aa73d050
suno81 <suno81@16fa5444-d079-11dd-a412-a1792178251d>
2009-01-12 14:36:51 +0000

    Simple JUnit test for creating concordance from one file

--- src/ru/suno/concordance/test/TestSeparatedSentences.java
+++ src/ru/suno/concordance/test/TestSeparatedSentences.java
@@ -4,18 +4,26 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import junit.framework.TestCase;
+
 import ru.suno.concordance.utils.PagesProcessor;
 
-public class TestSeparatedSentences {
+public class TestSeparatedSentences extends TestCase {
+
+	private static final String TEST_RESULT_FILENAME = "result";
+	public static final String CONCORDANCE_TESTDATA = "/home/suno/eclipse_workspace/Concordance/testdata/";
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		PagesProcessor pp = new PagesProcessor(null);
+	public void testSimpleFileParse() {
+		Utils.clearDir(new File(CONCORDANCE_TESTDATA), TEST_RESULT_FILENAME);
 		
-		pp.setDocPath("/home/suno/eclipse_workspace/Concordance/testdata/");
-		pp.setResultName("result");
+		PagesProcessor pp = new PagesProcessor(null);
+		assert(pp != null);
+		
+		pp.setDocPath(CONCORDANCE_TESTDATA);
+		pp.setResultName(TEST_RESULT_FILENAME);
 		
 		List<File> fileList = new ArrayList<File>();
 		fileList.add(new File("/home/suno/eclipse_workspace/Concordance/testdata/test1.doc"));
